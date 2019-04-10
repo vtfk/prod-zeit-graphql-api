6bcf68b37571a460c2e79b6fbbe89e91cf59ba7f
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-04-10 14:39:00 +0200

    Added requirement JWT auth and uploaded to now

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('now-env')
 
 module.exports = {
+  SERVICE_JWT: process.env.SERVICE_JWT || 'Louie Louie oh no I got to go Louie Louie oh no I got to go',
   DSF_SERVICE_URL: process.env.DSF_SERVICE_URL || 'https://dsf.service.io',
   DSF_SERVICE_JWT: process.env.DSF_SERVICE_JWT || 'Louie Louie oh no I got to go Louie Louie oh no I got to go',
   KOR_SERVICE_URL: process.env.KOR_SERVICE_URL || 'https://kor.service.io',
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const config = require('./config')
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const Dataloader = require('dataloader')
+const jwt = require('jsonwebtoken')
 const birthdateFromId = require('birthdate-from-id')
 const getAge = require('get-age')
 const getKor = require('./lib/lookup-kor')
@@ -20,9 +22,35 @@ try {
   const schema = require('./schemas/person-schema')
   const app = express()
 
-  // TODO: Add JWT auth
+  app.use('/', (req, res, next) => {
+    try {
+      const token = req.headers.authorization.replace(/bearer /i, '')
+      // Throws if invalid
+      jwt.verify(token, config.SERVICE_JWT)
+      
+    } catch (error) {
+      res.status(401).send("[401] Not authorized  --  Include correct JWT bearer token")
+      return
+    }
+    next()
+  })
 
-  app.use('/graphql', graphqlHTTP(req => {
+  app.use('/', (req, res, next) => {
+    try {
+      const token = req.headers.authorization.replace(/bearer /i, '')
+      const tokenDecoded = jwt.decode(token)
+
+      if (!tokenDecoded.exp || (tokenDecoded.exp - tokenDecoded.iat) > 600) {
+        throw Error('Token expiry time too long')
+      }
+    } catch (error) {
+      res.status(401).send("[401] Not authorized  --  Please include a token which expires in less than 10 minutes")
+      return
+    }
+    next()
+  })
+
+  app.use('/', graphqlHTTP(req => {
     return {
       schema: schema,
       context: createLoaders(),
@@ -30,9 +58,11 @@ try {
     }
   }))
 
-  app.listen(4000)
+  app.get('/')
+  //app.listen(4000)
 
-  console.log('Running a GraphQL API server at localhost:4000/graphql')
+  module.exports = app
+  console.log('Server started...')
 } catch (error) {
   throw error
 }
