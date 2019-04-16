ebf13e0bd00e05d03bcfb6d84f9e89b746e18ff1
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-04-16 14:54:17 +0200

    Moved JWT auth and dataloader out of index

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
-require('now-env')
+//require('now-env')
 
 module.exports = {
   SERVICE_JWT: process.env.SERVICE_JWT || 'Louie Louie oh no I got to go Louie Louie oh no I got to go',
+  SERVICE_JWT_EXPIRY_TIME: Number(process.env.SERVICE_JWT) || 120,
   DSF_SERVICE_URL: process.env.DSF_SERVICE_URL || 'https://dsf.service.io',
   DSF_SERVICE_JWT: process.env.DSF_SERVICE_JWT || 'Louie Louie oh no I got to go Louie Louie oh no I got to go',
   KOR_SERVICE_URL: process.env.KOR_SERVICE_URL || 'https://kor.service.io',
--- index.js
+++ index.js
@@ -1,54 +1,16 @@
-const config = require('./config')
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
-const Dataloader = require('dataloader')
-const jwt = require('jsonwebtoken')
-const birthdateFromId = require('birthdate-from-id')
-const getAge = require('get-age')
-const getKor = require('./lib/lookup-kor')
-const getDsf = require('./lib/lookup-dsf')
-
-// Caching per request.
-function createLoaders () {
-  return {
-    birthdateFromId: birthdateFromId,
-    getAge: getAge,
-    getKor: new Dataloader(keys => getKor(keys)),
-    getDsf: new Dataloader(keys => getDsf(keys, 'hentDetaljer'))
-  }
-}
+const jwtAuthVerify = require('./lib/tools/jwt-verify')
+const jwtAuthExpiry = require('./lib/tools/jwt-expiry')
+const createLoaders = require('./lib/tools/dataloaders')
 
 try {
   const schema = require('./schemas/person-schema')
   const app = express()
 
-  app.use('/', (req, res, next) => {
-    try {
-      const token = req.headers.authorization.replace(/bearer /i, '')
-      // Throws if invalid
-      jwt.verify(token, config.SERVICE_JWT)
-      
-    } catch (error) {
-      res.status(401).send("[401] Not authorized  --  Include correct JWT bearer token")
-      return
-    }
-    next()
-  })
+  app.use('/', jwtAuthVerify)
 
-  app.use('/', (req, res, next) => {
-    try {
-      const token = req.headers.authorization.replace(/bearer /i, '')
-      const tokenDecoded = jwt.decode(token)
-
-      if (!tokenDecoded.exp || (tokenDecoded.exp - tokenDecoded.iat) > 600) {
-        throw Error('Token expiry time too long')
-      }
-    } catch (error) {
-      res.status(401).send("[401] Not authorized  --  Please include a token which expires in less than 10 minutes")
-      return
-    }
-    next()
-  })
+  app.use('/', jwtAuthExpiry)
 
   app.use('/', graphqlHTTP(req => {
     return {
@@ -59,7 +21,7 @@ try {
   }))
 
   app.get('/')
-  //app.listen(4000)
+  app.listen(4000)
 
   module.exports = app
   console.log('Server started...')
