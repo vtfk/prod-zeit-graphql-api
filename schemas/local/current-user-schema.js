3f64a0897592f10e6f14cd8810070946e2e3e070
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-07-17 22:21:01 +0200

    Added current user schema

---
+++ schemas/local/current-user-schema.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+  GraphQLBoolean,
+  validateSchema
+} = require('graphql')
+/* eslint-enable no-unused-vars */
+const withAuth = require('graphql-auth').default
+
+const { PersonType } = require('./schema-type-person')
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: () => ({
+      user: {
+        description: 'Contains the current user\'s person object, if using a valid AAD token',
+        type: PersonType,
+        resolve: withAuth( ['AAD-USER'], (root, args, context) => {
+          if (!context.auth.session.personalId) throw Error(`Context did not contain a personalId`)
+          const personalId = context.auth.session.personalId
+          context.logger('info', ['person-schema', 'Start'])
+
+          // Check if id is exactly 11 digits
+          if (!(RegExp(/^\d{11}$/).exec(personalId))) {
+            context.logger('error', ['person-schema', 'id-wrong-format'])
+            throw Error(`User\'s personalId is not 11 chars or contains non-integer characters`)
+          }
+
+          return { id: personalId }
+        })
+      }
+    })
+  })
+})
+
+// Schema validation, throws if error.
+const schemaErrors = validateSchema(schema)
+if (schemaErrors.length > 0) {
+  throw Error(`Error in schema: \n${schemaErrors}`)
+}
+
+module.exports = schema
--- schemas/merge-schemas.js
+++ schemas/merge-schemas.js
@@ -1,10 +1,13 @@
 const { mergeSchemas } = require('graphql-tools')
+const { logger } = require('../lib/tools/logger')
 const promisedSchemas = [
   require('./local/person-schema'),
-  require('./local/auth-schema')
+  require('./local/auth-schema'),
+  require('./local/current-user-schema')
 //  require('./remote-schemas/fint')()
 ]
 module.exports = async () => {
+  logger('info', ['Merging schemas', promisedSchemas.length])
   const schemas = await Promise.all(promisedSchemas)
 
   const schema = mergeSchemas({
